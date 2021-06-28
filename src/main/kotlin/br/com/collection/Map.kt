a851fe90fe398d74fd9e6d5e18331bc3d849a83e
jenifer-mathias <jenifer.mathias.santos@hotmail.com>
2021-06-28 09:45:54 -0300

    Add example Map

---
+++ src/main/kotlin/br/com/collection/Map.kt
@@ -0,0 +1,17 @@
+package br.com.collection
+
+fun main() {
+    val pair: Pair<String, Double> = Pair("João", 1000.0)
+    val map1 = mapOf(pair)
+
+    // consumer
+    map1.forEach { (k, v) -> println("Chave: $k - Valor: $v") }
+
+    // infix
+    val map2 = mapOf(
+        "Carmen" to 2500.0,
+        "Maria" to 3000.0
+    )
+
+    map2.forEach { (k, v) -> println("Chave: $k - Valor: $v") }
+}
